4d652bb81418d386166f9cb3fc7b6f8a9916a4d7
Kid <kidonng@gmail.com>
2021-05-10 22:04:45 +0800

    Remove unnecessary `-c` in `complete`

--- completions/trash.fish
+++ completions/trash.fish
@@ -1,4 +1,4 @@
-complete -c trash -x -s l -l list -d "List files in the Trash"
-complete -c trash -x -s e -l empty -d "Empty the Trash"
-complete -c trash -x -s v -l verbose -d "Show files being trashed"
-complete -c trash -x -s h -l help -d "Show help message"
+complete trash -x -s l -l list -d "List files in the Trash"
+complete trash -x -s e -l empty -d "Empty the Trash"
+complete trash -x -s v -l verbose -d "Show files being trashed"
+complete trash -x -s h -l help -d "Show help message"
